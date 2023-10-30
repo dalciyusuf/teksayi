0c851fa35a68b6e6063fc517f8ce0b4c02bc1e38
Yusuf <115079732+dalciyusuf@users.noreply.github.com>
2023-10-30 18:09:57 +0300

    daire.cpp

--- daire.cpp
+++ daire.cpp
@@ -1,57 +1,26 @@
 #include <stdio.h>
-#define pi 3.14
 
 main()
 {
-	float r,cevre,alan;
+	//Dışarıdan girilen bir n tam sayısına kadar olan tek tam sayıların toplamını bulan c programı.
 	
-	printf("dairenin yaricapini giriniz:	");
-	scanf("%f",&r);
-	
-	cevre= 2*pi*r;
-	alan = pi*r*r;
-	
-	printf("cevre=%f alan=%f\n",cevre,alan);
-	
-	
-	
-	
-	
-	
-	
-	
+	int i,n,sonuc=0;
+	printf("bir sayi giriniz:	");
+	scanf("%d",&n);
+	i=1;
+		
+	while(i<=n)
+	{
+		
+		sonuc = sonuc +i;
+		i=i+2;
+		
+		
+	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	printf("n sayisina kadar olan tek sayilarin toplami=	%d",sonuc);
+
+
 	return 0 ;
 }
